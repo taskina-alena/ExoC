51685581b2c66e0b1398e695e92a8f26060edb9f
taskina-alena <51115325+taskina-alena@users.noreply.github.com>
2021-03-01 17:26:15 +0700

    Update get_BAF.sh

--- get_BAF.sh
+++ get_BAF.sh
@@ -3,9 +3,10 @@ java -jar ~/picard.jar AddOrReplaceReadGroups -I bam/$sample.bam.sorted -O bam/$
 #this command add group to bam file and allows to launch CollectAllelicCounts (names of group are arbitary)
 N=4; 
 #number of parallel threads, each thread takes ~3 cores
+#was stolen from https://gist.github.com/mike-seger/cb3e05dc41f250e713ad941543a676a1
 (
 #for contig in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY; do
-for contig in chr22 do
+for contig in chr22; do
         ((i=i%N)); ((i++==0)) && wait 
         gatk --java-options '-Xmx16g' CollectAllelicCounts -L $contig -I bam/$sample.tagged.bam -R hg19_canonic/hg19.fa -O $sample.$contig.tsv &
         #the main command, output is tsv file with headers and 6 columns: CONTIG, POSITION,	REF_COUNT,	ALT_COUNT, REF_NUCLEOTIDE,	ALT_NUCLEOTIDE
