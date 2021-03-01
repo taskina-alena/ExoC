49730f7f1ab465c1ff3616e18a2d8296c0233e7d
taskina-alena <51115325+taskina-alena@users.noreply.github.com>
2021-03-01 17:20:00 +0700

    Update get_BAF.sh

--- get_BAF.sh
+++ get_BAF.sh
@@ -7,9 +7,9 @@ N=4;
 #for contig in chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY; do
 for contig in chr22 do
         ((i=i%N)); ((i++==0)) && wait 
-        gatk --java-options '-Xmx16g' CollectAllelicCounts -L $contig -I bam/s41.tagged.bam -R hg19_canonic/hg19.fa -O s41.$contig.tsv &
+        gatk --java-options '-Xmx16g' CollectAllelicCounts -L $contig -I bam/$sample.tagged.bam -R hg19_canonic/hg19.fa -O $sample.$contig.tsv &
         #the main command, output is tsv file with headers and 6 columns: CONTIG, POSITION,	REF_COUNT,	ALT_COUNT, REF_NUCLEOTIDE,	ALT_NUCLEOTIDE
-        awk '{OFS="\t"; if ($3+$4>0) print $1,$2,$2+1,$4/($3+$4)}' s41.$contig.tsv > s41.$contig.bedGraph
+        awk '{OFS="\t"; if ($3+$4>0) print $1,$2,$2+1,$4/($3+$4)}' $sample.$contig.tsv > $sample.$contig.bedGraph
         #output is bedGraph with 4 columns: contig, pos, pos+1, B-allelic frequency (ratio of ALT_COUNT to REF_COUNT+ALT_COUNT)
         #only positions with non-zero coverage are written ($3+$4>0)
 done
