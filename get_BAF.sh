ed66d7bba0a2bc61e56cacba82c57ec9f1478ef1
taskina-alena <51115325+taskina-alena@users.noreply.github.com>
2021-06-06 11:09:36 +0700

    Update get_BAF.sh

--- get_BAF.sh
+++ get_BAF.sh
@@ -1,14 +1,15 @@
 sample=$1
 java -jar ~/picard.jar AddOrReplaceReadGroups -I bam/$sample.bam.sorted -O bam/$sample.tagged.bam -RGID 4 -RGLB lib1 -RGPL ILLUMINA -RGPU unit1 -RGSM 41
 #this command add group to bam file and allows to launch CollectAllelicCounts (names of group are arbitary)
-N=1
+N=$2
 #number of parallel threads, each thread takes ~3 cores
 
-for contig in $2-$($#); do
-        ((i=i%N)); ((i++==0)) && wait 
-        echo $i
-        gatk --java-options '-Xmx16g' CollectAllelicCounts -L $contig -I bam/$sample.tagged.bam -R hg19_canonic/hg19.fa -O $sample.$contig.tsv --verbosity ERROR&
-        #the main command, output is tsv file with headers and 6 columns: CONTIG, POSITION,	REF_COUNT,	ALT_COUNT, REF_NUCLEOTIDE,	ALT_NUCLEOTIDE
+for contig in $@; do
+        if [[ $contig =~ chr.* ]]; then
+		((i=i%N)); ((i++==0)) && wait 
+        	gatk --java-options '-Xmx16g' CollectAllelicCounts -L $contig -I bam/$sample.tagged.bam -R hg19_canonic/hg19.fa -O $sample.$contig.tsv --verbosity ERROR&
+        	#the main command, output is tsv file with headers and 6 columns: CONTIG, POSITION,	REF_COUNT,	ALT_COUNT, REF_NUCLEOTIDE,	ALT_NUCLEOTIDE
+	fi
 done
 
 wait
